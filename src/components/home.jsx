d2b8a46e5d3b9d40dc4dd8cd2801e366e7ecbcce
Moyinoluwa360 <ogundanajoseph17@gmail.com>
2024-09-29 18:11:56 +0100

    removed section from home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -9,15 +9,6 @@ const Home = (props) => {
   return (
     <Container>
       {!props.user && <Navigate to="/" replace/>}
-      <Section>
-        <h5>
-          <a>Hiring in a hurry? - </a>
-        </h5>
-        <p>
-          Find talented pros in record time with Upwork and keep business
-          moving.
-        </p>
-      </Section>
       <Layout>
         <Leftside />
         <Main />
@@ -38,33 +29,6 @@ const Content = styled.div`
   margin-right: auto;
 `;
 
-const Section = styled.section`
-  min-height: 50px;
-  padding: 16px 0;
-  box-sizing: content-box;
-  text-align: center;
-  text-decoration: underline;
-  display: flex;
-  justify-content: center;
-  h5 {
-    color: #0a66c2;
-    font-size: 14px;
-    a {
-      font-weight: 700;
-    }
-  }
-
-  p {
-    font-size: 14px;
-    color: #434649;
-    font-weight: 600;
-  }
-
-  @media (max-width: 768px) {
-    flex-direction: column;
-    padding: 0 5px;
-  }
-`;
 
 const Layout = styled.div`
   display: grid;
